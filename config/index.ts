657bfc0719ef678f84ef6efda9ea8466cb6990cf
bfpbr5 <bffpbr5@gmail.com>
2023-08-01 17:36:11 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import { AppInfo } from "@/types/app"
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = 'a619a8f5-2ab9-4c90-b799-7b935830071e'
+export const API_KEY = 'app-CsXTaZQOtSMPJWi4xK94EVB7'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   "title": 'Chat APP',
