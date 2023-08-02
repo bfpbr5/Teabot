2ccf9f73ec0752c2bbf81c3ae128068fcbf003d9
bfpbr5 <bffpbr5@gmail.com>
2023-08-02 20:07:12 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = 'a619a8f5-2ab9-4c90-b799-7b935830071e'
 export const API_KEY = 'app-CsXTaZQOtSMPJWi4xK94EVB7'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  "title": 'Chat APP',
+  "title": '茶博士 APP',
   "description": '',
-  "copyright": '',
+  "copyright": '数字极光',
   "privacy_policy": '',
   "default_language": 'zh-Hans'
 }
